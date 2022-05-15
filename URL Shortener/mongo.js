a9aa474d0a78ab519257f5ed310a30be3829a295
Isadora Ariane <65814808+sdrariane@users.noreply.github.com>
2022-05-15 11:15:26 -0300

    Add files via upload

---
+++ URL Shortener/mongo.js
@@ -0,0 +1,6 @@
+mongoose.connect("mongodb://127.0.0.1:27017/SUCCESS",{
+        useCreateIndex:true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true}).then(()=> {
+console.log('Database Successfully Connected')},error =>{
+console.log(error)})
